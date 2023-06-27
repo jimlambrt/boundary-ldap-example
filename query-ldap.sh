3ddd96d9191e2d2be83f756a740c70734dccc2c4
Jim <jim.lambert@gmail.com>
2023-06-27 17:08:14 -0400

    chore: add bin/sh

--- query-ldap.sh
+++ query-ldap.sh
@@ -1,3 +1,6 @@
-ldapsearch -w password -D "cn=read-only-admin,dc=example,dc=com" -b "dc=example,dc=com" -x -H "ldap://ldap.forumsys.com"  "uid=einstein"
+#!/bin/sh
 
+# search for the einstein user
+ldapsearch -w password -D "cn=read-only-admin,dc=example,dc=com" -b "dc=example,dc=com" -x -H "ldap://ldap.forumsys.com"  "uid=einstein"
+# search for the Scientists group
 ldapsearch -w password -D "cn=read-only-admin,dc=example,dc=com" -b "dc=example,dc=com" -x -H "ldap://ldap.forumsys.com"  "ou=Scientists"
